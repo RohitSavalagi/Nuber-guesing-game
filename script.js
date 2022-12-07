dd693908ea72f8c8b0f3a3c5e9a15f1a05b16267
Rohit Savalagi <rohit.savalagi@netcracker.com>
2022-12-07 11:00:39 +0530

    Changes have been made, and a new slider has been created to help users select the number of restricted guesses they desire.

--- script.js
+++ script.js
@@ -1,246 +1,221 @@
 //==========================variables===========================================
 
-var remain = 5;
 
 //==========================dom selectors
-var myRange = document.getElementById("myrange");
-const remaining = document.querySelector('#lastResult');
-var returnResult = document.getElementById('currentResult');
-const startOver = document.getElementById("newStarting");
-var returnHistory = document.getElementById('resultHistory');
+var myRange = document.getElementById("my-range");
+const remaining = document.querySelector("#last-result");
+var returnResult = document.getElementById("current-result");
+const startOver = document.getElementById("new-starting");
+var returnHistory = document.getElementById("result-history");
+var chances = document.getElementById("guess-range")
 //Selecting By Tag
 var label = document.getElementsByTagName("label")[1];
 //Input text feild
-let getInputValue = document.getElementById("getGuess")
+let getInputValue = document.getElementById("get-guess");
 
-//==========================creating dom elements 
+//==========================creating dom elements
 
-const p = document.createElement('p');
+const p = document.createElement("p");
+let div = document.createElement("div");
+let alertDiv = document.createElement("div");
 var max = myRange.value;
 
-console.log("max value is set to " + max)
 var rand = getRandom(0, max);
-document.getElementById("randBox").innerHTML = `<span>${rand}<span>`;
-
 
 //===================variables
+var remain = parseInt(Math.ceil(Math.log(max))) + 1;
+chances.max = remain;
 
 var numOfGuess = 1;
-let playGame = true;
-
-
-
-
 //====================buttons
-let submit = document.getElementById("sub")
-submit.disabled = true
-let restartBtn = document.getElementById('reset')
+let submit = document.getElementById("submit-button");
 
+let restartBtn = document.getElementById("reset-button");
 
 
 
 //functionalities
-restartBtn.disabled = true
 
-
-//=====================================Main Functions==========================================
-//===============Starting Game================================
-
-if (playGame) {
-    submit.addEventListener("click", function (e) {
-        e.preventDefault();
-        const guess = parseInt(getInputValue.value);
-        validateGuess(guess);
-    })
-};
-//====================================End Main Funactio========================================
-
-
-//====================================Supporting Functions=====================================
-
-//=======Input validation 
-
-function validateGuess(guess) {
-    
-    
-    if (isNaN(guess)) {
-        alert('Please Enter a valid Number');
-    }
-    else if (guess < 1) {
-        alert('Please enter number greater than 0');
-    }
-    
-    else if (guess > max) {
-        alert("please Enter a value lesser than" + (++max));
-    }
-    
-    else {
-     
-        displayGuesses(guess);
-        checkGuess(guess);
-    }
+function submitClick() {
+  const guess = parseInt(getInputValue.value);
+  validateGuess(guess);
 }
 
+//=======Input validation
+
+function validateGuess(guess) {
+  if (isNaN(guess)) {
+    alert("Please Enter a valid Number");
+  } else if (guess <= 0) {
+    alert("Please enter number greater than or equal to 0");
+  } else if (guess >= max) {
+    alert("please Enter a value lesser than or equal to" +max);
+  } else {
+    displayGuesses(guess);
+    checkGuess(guess);
+  }
+}
 
 //=======Check the Guess is correct or not and closness
 
 function checkGuess(guess) {
-    var flag = 0;
-    
-    console.log(remain)
-    if (guess == rand) {
-        returnResult.innerHTML =
-        '<div class="alert alert-success">Awesome job, you got it!</div>';
-        endGame();
-        flag = 1;
-    }
-    else if (guess < rand ) {
-        returnResult.innerHTML =
-        '<div class="alert alert-warning">Your guess is low!</div>';
-    }
-    else if (guess > rand ) {
-        returnResult.innerHTML =
-        '<div class="alert alert-warning">Your guess is high!</div>';
-    }
-    
-    // if Guess Crosses 5 and 
-    if (numOfGuess >= (remain) && flag == 0) {
-        flag = 0;
-        returnResult.innerHTML =
-        `<div class="alert alert-lost">You Lost the Match <br>Number was ${rand}</div>`;
-        endGame();
-    }
-    myRange.disabled = true;
+  let flag = 0;
+  if (guess == rand) {
+    alertDiv.innerText = "Awesome job, you got it!";
+    alertDiv.classList.add("alert", "alert-success");
+    returnResult.append(alertDiv);
+    endGame();
+    flag = 1;
+  } else if (guess < rand) {
+    alertDiv.innerText = "Your guess is low!";
+    alertDiv.classList.add("alert", "alert-warning");
+    returnResult.append(alertDiv);
+  } else if (guess > rand) {
+    alertDiv.innerText = "Your guess is high!";
+    alertDiv.classList.add("alert", "alert-warning");
+    returnResult.append(alertDiv);
+  }
+
+  // if Guess Crosses 5 and
+  if (numOfGuess >= remain && flag == 0) {
+    flag = 0;
+    alertDiv.innerHTML = `You Lost the Match <br>Number was ${rand}`;
+    alertDiv.classList.add("alert", "alert-lost");
+    returnResult.append(alertDiv);
+    //returnResult.innerHTML = `<div class="alert alert-lost">You Lost the Match <br>Number was ${rand}</div>`;
+    endGame();
+  }
 }
 
 //Pushing and Showing  History
 //Updating No Of Guesses
 function displayGuesses(guess) {
-    restartBtn.disabled = false;
-    getInputValue.value = '';
-    var createHistoryElement = document.createElement('div');
-    createHistoryElement.classList.add('list-group-item');
-    createHistoryElement.innerText =  guess;
-    returnHistory.append(createHistoryElement);
-    numOfGuess++;
-    remaining.innerHTML = `${remain - numOfGuess} `;
-    myRange.disabled = true;
+  restartBtn.disabled = false;
+  getInputValue.value = "";
+  div.classList.add("list-group-item");
+  div.innerText += guess + "\n";
+  returnHistory.appendChild(div);
+  numOfGuess++;
+  remaining.innerHTML = `${remain - numOfGuess} `;
+  myRange.disabled = true;
+  chances.style.visibility = "hidden"
 }
 
-//Ending Game Win Or Lose 
-
+//Ending Game Win Or Lose
 
 function endGame() {
-    
-    getInputValue.value = "";
-    getInputValue.setAttribute('disabled', '');
-    p.classList.add('button');
-    p.innerHTML = `<button id="newGame" class="newGame">Start New Game</button>`
-    startOver.appendChild(p);
-    restartBtn.disabled = true
-    console.log("disabled")
-    document.getElementById('resultHistory').innerHTML = "";
-    newGame();
-    submit.disabled = true
+  getInputValue.value = "";
+  getInputValue.setAttribute("disabled", "");
+  p.classList.add("button");
+  p.innerHTML = `<button id="newGame" class="newGame" onclick="newStarting()">Start New Game</button>`;
+  startOver.appendChild(p);
+  restartBtn.disabled = true;
+
+//   newGame();
+  submit.disabled = true;
 }
-
-
-
 
 //After Loosing or Wining  => Starting a New Match
 
 //Adding an Event Listner
-function newGame() {
-    document.getElementById('newGame').addEventListener('click', newStarting, false)
-}
+// function newGame() {
+//   newStarting
+// }
 
 //function according to event
 function newStarting() {
-    submit.disabled = false
-    newreset()
-    reviseMax();
-    startOver.removeChild(p);
+  commonLinesInNewStartingResetFunction();
+  startOver.removeChild(p);
 }
 
-//After Clicking Reset Button 
+//After Clicking Reset Button
 
-function reseting() {
-   newreset()
-    reviseMax()
+function reset() {
+    commonLinesInNewStartingResetFunction();
 }
 
-function newreset(){
-    rand = parseInt((Math.random() * 100) + 1);
-    console.log(rand)
-    numOfGuess = 1;
-    remain = parseInt(Math.floor(Math.log(max))) + 1
-    returnResult.innerHTML = ""
-    document.getElementById('resultHistory').innerHTML = "";
-    getInputValue.removeAttribute('disabled');
-    restartBtn.disabled = true
-    label.click();
-    myRange.disabled = false;
-    myRange.value = 100;
-    document.getElementById("valBox").innerHTML = "100"
-    getInputValue.value = ""
-    submit.disabled = true
+function commonLinesInNewStartingResetFunction() {
+  const classes = ["alert", "alert-lost", "alert-success", "alert-warning"];
+  classes.forEach((c) => {
+    if (alertDiv.classList.contains(c)) {
+      alertDiv.classList.remove(c);
+    }
+  });
+  returnHistory.firstChild.innerText = "";
+  returnHistory.removeChild(returnHistory.firstChild);
+  rand = parseInt(Math.random() * 100 + 1);
+  numOfGuess = 1;
+  remain = parseInt(Math.ceil(Math.log(max))) + 1;
+  chances.max = remain
+  chances.value = remain
+  returnResult.innerHTML = "";
+  getInputValue.removeAttribute("disabled");
+  restartBtn.disabled = true;
+  label.click();
+  myRange.disabled = false;
+  chances.style.visibility = 'visible';
+  myRange.value = 100;
+  chances.value = 5;
+  document.getElementById("value-box").innerHTML = "100";
+  getInputValue.value = "";
+  submit.disabled = true;
+  reviseMax();
 }
-
 
 //Key Press Code
 
-getInputValue.addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      submit.click();
-    }
-  });
+getInputValue.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    submit.click();
+  }
+});
 
 //showing value in range value
 
-function showVal(newVal) {
-    document.getElementById("valBox").innerHTML = newVal;
-    max = myRange.value;
-    console.log("Now Max value is revised according to range")
-    remain = parseInt(Math.floor(Math.log(max))) + 1;
-    remaining.innerHTML = `${remain - numOfGuess} `;
-    rand = getRandom(0, max);
-    document.getElementById("randBox").innerHTML = `${rand}`;
-    console.log(rand + " is the random number")
-    document.getElementById("valBox").innerHTML = `<p>${max}</p>`;
+function ShowValue(newVal) {
+  document.getElementById("value-box").innerHTML = newVal;
+  max = myRange.value;
+  remain = parseInt(Math.ceil(Math.log(max))) + 1;
+  chances.max = remain;
+  remaining.innerHTML = `${remain - numOfGuess} `;
+  rand = getRandom(0, max);
+  document.getElementById("value-box").innerHTML = `<p>${max}</p>`;
+  chances.max = remain
+  chances.value = remain
 }
 
-function reviseMax(){
-    max = myRange.value;
-    console.log("Now Max value is revised according to range")
-    remain = parseInt(Math.floor(Math.log(max))) + 1;
-    remaining.innerHTML = `${remain - numOfGuess} `;
-
+function reviseMax() {
+  max = myRange.value;
+  remain = parseInt(Math.ceil(Math.log(max))) + 1;
+  chances.max = remain
+  chances.value = remain
+  remaining.innerHTML = `${remain - numOfGuess} `;
 }
-
-
 
 //=====Geting Random number
 
-function getRandom(_min , _max){
-    document.getElementById("randBox").innerHTML = `<span>${rand}<span>`;
-    return parseInt(Math.random() * _max)
+function getRandom(_min, _max) {
+  return parseInt(Math.random() * _max);
 }
 
-//Diabling the range 
+//Diabling the range
 
-function _disable(){
-   
-    myRange.disabled = true;
-    console.log("Range is Disabled")
-
-
+function disableRangeElement() {
+  myRange.disabled = true;
 }
 
-function enableSub(){
-    submit.disabled = false;
+function enableSubmitButton() {
+  submit.disabled = false;
 }
 
 
+function ShowUpdatedChances(val){
 
+    remaining.innerText = val - 1;
+    remain = val;
+}
 
+function updateGuessRangeSlider(ele){
+  document.getElementById('guessRange').value = ele
+}
